958df15deb76f0e473815f204535d6e1601f2d58
Rico <drraccoony@gmail.com>
2022-07-12 23:49:14 -0500

    Cleanup and comments

--- app/Models/logged_activities.php
+++ app/Models/logged_activities.php
@@ -9,16 +9,25 @@ class logged_activities extends Model
 {
     use HasFactory;
 
+    /**
+     * Create the activity relationship lookup
+     */
     public function activity()
     {
         return $this->belongsTo(activities::class);
     }
 
+    /**
+     * Create the user relationship lookup
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Create the team relationship lookup
+     */
     public function team()
     {
         return $this->belongsTo(team::class);

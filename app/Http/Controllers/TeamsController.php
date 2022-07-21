67cdc0a9a986954ba6408339a622267796b1d989
Rico <drraccoony@gmail.com>
2022-07-20 21:56:15 -0500

    New teams page

---
+++ app/Http/Controllers/TeamsController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\activities;
+use App\Models\team;
+
+class TeamsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $teams = team::query()
+            ->get();
+
+        return view('teams.index')
+            ->with(compact('teams'));
+    }
+
+    public function create(Request $request)
+    {
+
+        return view('teams.create', $request)
+            ->with(compact('request'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' => ['required','string'],
+        ]);
+        $post = new Activities;
+        $post->name = $request->name;
+        $post->locked = $request->locked;
+        $post->save();
+        return redirect('activities');
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ActivitiesController;
 use App\Http\Controllers\ActivityLogController;
+use App\Http\Controllers\TeamsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,13 +16,41 @@ use App\Http\Controllers\ActivityLogController;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/dashboard');
+
+/*
+|--------------------------------------------------------------------------
+| Auth User Routes
+|--------------------------------------------------------------------------
+|*/
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return redirect('/dashboard');
+    });
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/log/mine', [ActivityLogController::class, 'mine'])
+        ->name('activity_log.mine');
+
+    Route::get('/teams', [TeamsController::class, 'index'])
+        ->name('teams');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    Route::get('/activities', [ActivitiesController::class, 'index'])
+        ->name('activities');
+    Route::get('/activities/create', [ActivitiesController::class, 'create'])
+        ->name('activities.create');
+    Route::post('/activities/create', [ActivitiesController::class, 'store'])
+        ->name('activities.store');
+
+    Route::get('/teams/create', [TeamsController::class, 'create'])
+        ->name('teams.create');
+    Route::post('/teams/create', [TeamsController::class, 'store'])
+        ->name('teams.store');
+
 
 Route::get('/activities', [ActivitiesController::class, 'index'])
     ->middleware(['auth'])->name('activities');

66d471fafa614ed0844b0ce270a3eef5f9bf92bb
Shawn McHenry <drraccoony@gmail.com>
2022-09-06 14:14:59 -0500

    Admit edit ability for editing users (#19)
    * Admit edit ability for editing users
    
    * Team modifications from an admin

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Team;
 
 class UsersController extends Controller
 {
@@ -20,22 +21,26 @@ class UsersController extends Controller
             ->with(compact('users'));
     }
 
-    public function edit(Request $request)
-    {
-        $user = $request->user();
+    // public function edit(Request $request)
+    // {
+    //     $user = $request->user();
+    //     $teams = Team::query()
+    //         ->get();
         
-        return view('users.edit')
-            ->with(compact('user'));
-    }
+    //     return view('users.edit')
+    //         ->with(compact('user','teams'));
+    // }
 
     public function editUser(Request $request, User $user)
     {
         if ($request->user()->cannot('update', $user)) {
             return redirect()->route('user.edit');
         }
+        $teams = Team::query()
+            ->get();
 
         return view('users.edit')
-            ->with(compact('user'));
+            ->with(compact('user','teams'));
     }
 
     public function confirm(Request $request, User $user)
@@ -62,5 +67,24 @@ class UsersController extends Controller
         }
 
         return redirect()->route('users');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => ['required','string'],
+        ]);
+
+        $user->update($request->all());
+
+        return redirect()
+            ->route('users');
     }
 }
